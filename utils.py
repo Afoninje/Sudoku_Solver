37ca00ab654e7b07e7d01afdc72a014bc19e30f8
Pruthvi Pursnaney <55634510+Afoninje@users.noreply.github.com>
2020-05-26 04:43:48 +0530

    Remove all easy to remove number
    All the single numbers present in a row , column and 3*3 grids get removed from the possible outcomes of the same row,column and 3*3 grid

--- utils.py
+++ utils.py
@@ -14,11 +14,11 @@ def createDict(grid,sudokuString):
 
     return sudokuDict
 
-def display(grid,sudokuDict):
+def display(grid,sudoku):
     print()
     counter = 1
     for i in range(81):
-        print(sudokuDict[grid[i]],end =" ")
+        print(sudoku[grid[i]],end =" ")
         if counter%9==0:
             print()
         elif counter%3==0:
@@ -37,18 +37,43 @@ def eliminate(grid,sudoku):
             row = grid[i][0]
             column = grid[i][1]
             
+            #Removes the number from the rows
             for i in range(1,10):
                 currentRow = row+str(i)
                 if(len(sudoku[currentRow]))>1:
-                    print(currentRow,value)
-                    sudoku[currentRow] = sudoku[currentRow].replace(value,"")
+                    sudoku[currentRow] = sudoku[currentRow].replace(value," ")
 
+            #Removes the number from the columns
             rows = ["Dummy","A","B","C","D","E","F","G","H","I"]
             for i in range(1,10):
                 currentColumn = rows[i]+column
-                if(len(sudoku[currentColumn]))>1:
-                    sudoku[currentColumn] = sudoku[currentColumn].replace(value,"")
+                if len(sudoku[currentColumn])>1:
+                    sudoku[currentColumn] = sudoku[currentColumn].replace(value," ")
 
+            #Remove the number from the 3*3 mini-grids in the sudoku
+            if row == "A" or row == "B" or row == "C":
+                minigrid_row="ABC"
+            elif row == "D" or row == "E" or row == "F":
+                minigrid_row="DEF"
+            elif row == "G" or row == "H" or row == "I":
+                minigrid_row="GHI"
+
+            if column == "1" or column == "2" or column == "3":
+                minigrid_column="123"
+            elif column == "4" or column == "5" or column == "6":
+                minigrid_column="456"
+            elif column == "7" or column == "8" or column == "9":
+                minigrid_column="789"   
+
+            mini_grids= createGrid(minigrid_row,minigrid_column)
+
+            for i in range(9):
+                if len(sudoku[mini_grids[i]])>1:
+                    sudoku[mini_grids[i]] = sudoku[mini_grids[i]].replace(value," ")
+
+    #for loop to remove all the trailing spaces
+    for i in range(81):
+        sudoku[grid[i]] = sudoku[grid[i]].replace(" ","")
     return sudoku
 
 
@@ -60,12 +85,6 @@ def main():
 
     grid = createGrid(rows, cols) 
     # Create a grid from A1 to I9
-    row_units = [createGrid(r, cols) for r in rows] 
-    # Create row. Ex = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9']
-    column_units = [createGrid(rows, c) for c in cols] 
-    # Create column. Ex = ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1']
-    square_units = [createGrid(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')] 
-    # Create the sub-grids. Ex = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']
 
     #sudokuString = input("Enter the sudoku in a string format")
     sudokuString = "..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3.."
